64ea8a2272f518f7761bc31d83dfe1be7ab5bdfc
Bradley Grose <bradley@grose.net>
2020-09-08 00:13:32 -0400

    Update homework1.asm

--- homework1.asm
+++ homework1.asm
@@ -7,7 +7,7 @@ addi   $s1, $t1, -23		# g = $t1 - 23
 
 # C Code A[i+1] = g + A[12] + 1
 
-lw		  $t0, 48($s0)		  # load A[12]
+lw		 $t0, 48($s0)		  # load A[12]
 add		$t1, $s1, $t0		# $t0 = g + $t0
 addi	$t1, $t1, 1			  # $t1 = $t1 + 1
 addi	$t2, $s2, 1			 # [i+1]
