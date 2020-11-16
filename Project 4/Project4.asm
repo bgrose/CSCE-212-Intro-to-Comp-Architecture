c0f9cfe300040c2738ded133aa632e3cd61fc4b8
bgrose <47512745+bgrose@users.noreply.github.com>
2020-11-16 16:09:44 -0500

    Added Comments and Cleanup

--- Project 4/Project4.asm
+++ Project 4/Project4.asm
@@ -107,6 +107,7 @@ sub.s $f3,$f3,$f7	# f=(0.1*A^4)-(0.2*B^3)+(0.3*C^2)-(0.4*D)
 
 ########################
 # Print Out F using sys call
+# f is stored in f3
 ########################
 
 li $v0, 4	# Sets v0 to 4 for String
@@ -158,10 +159,12 @@ mov.s $f5, $f2  #Result into F4
 ####
 # Add it all Together using add.s
 ####
+
 add.s $f3, $f4, $f5	# g = (0.1×AB^2) + (0.2×C^2D^3)
 
 ########################
 # Print Out G using syscall
+# g is sotrd in f3
 ########################
 
 li $v0, 4	# Sets v0 to 4 for String
@@ -174,16 +177,22 @@ syscall		# prints value
 j end		# Sends to the end of Program
 
 #######################################################
-# METHODS
+# PROCEDURES
+# Includes: a Procedure that raises an int to a power
+#	    a Procedure that multiplies 2 integers
+#           a Procedure that lets you multiple a float and a integer
 #######################################################
 
 ####
 # Power, takes a integer and a power and loops through and adddition
+# S0 is where the integer is passed in
+# S1 is where the exponent is passed in
+# s2 is where the result stores
 ###
 power:
-li $t6, 1	#Loop Counter
-li $s2, 0	#Output Value
-move $s3, $s0	#Moves value to S3
+li $t6, 1		#Loop Counter
+li $s2, 0		#Output Value
+move $s3, $s0		#Moves value to S3
 Loop1:
 slt $t0, $t6, $s1 	   #Makes T0 1 if its less then t6
 beq $t0, $zero, doneFinal  #If T0 is 0, go to end
@@ -196,20 +205,22 @@ add $s2, $s2, $s3	  # Adds s2 and s3 to compound
 addi $t5, $t5, 1		  #Internal loop ++
 j Loop2			  #Creates the loop
 done:
-move $s3, $s2		 #Moves sum to s3
-addi $t6, $t6, 1	         #$t6++
-j Loop1			 #Loop back to Loop1
+move $s3, $s2		  #Moves sum to s3
+addi $t6, $t6, 1	          #$t6++
+j Loop1			  #Loop back to Loop1
 doneFinal:
-jr $ra			#Returns the value 
+jr $ra			  #Returns the value 
 
 ####
 # multipFloat allows a float decimal to be multiplied 
 # by the value sent in in s1
+# f1 will be the number that multiplies to get the value through addition
+# f2 will be the output value
 ###
 
 multipFloat:
-li $t5, 0	#Resets Loop for Multiplication
-l.s $f2, zer	#Sets output to 0.0
+li $t5, 0		#Resets Loop for Multiplication
+l.s $f2, zer		#Sets output to 0.0
 Loop3:
 slt $t0,$t5,$s1		#Set $t4 = 1 if t5 is less then s1		
 beq $t0,$zero,doneFP	#if t0 is 0, exit		
@@ -222,22 +233,24 @@ jr $ra
 ####
 # multipInts allows for 2 integers passes in in s1 to be multiplied togehter 
 # to get the answe value
+# takes in value in s1 for the times multipled
+# takes in value in s0 for the other int to be multiplied
 ###
 
 multipInts:
 li $t5,0 		#Loop to 0	
 li $s2,0			#Return to 0
 Loop4:			
-slt $t0,$t5,$s1			# if $t5 < $f1, t0 = 1
-beq $t0,$zero,doneInt		# If T0 is 0, then return value
-add $s2,$s2,$s0			# s2 += s0
-addi $t5,$t5,1			# i++ for the loop
-j Loop4				# Loop back around
+slt $t0,$t5,$s1		# if $t5 < $f1, t0 = 1
+beq $t0,$zero,doneInt	# If T0 is 0, then return value
+add $s2,$s2,$s0		# s2 += s0
+addi $t5,$t5,1		# i++ for the loop
+j Loop4			# Loop back around
 doneInt: 			
 jr $ra			# Returns to call
 
 #################
-# End (Skips the Methods)
+# End (Skips the Methods) and terminates the product
 #################
 
 end: 		#Jumps past all the methods
